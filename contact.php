32e81689939a535bdf93599956755cb2cc48e506
Paul Burge <paul.burge8@gmail.com>
2014-06-24 01:20:05 -0400

    contact form broke. i hope this fixes it

--- contact.php
+++ contact.php
@@ -3,28 +3,30 @@ $field_name = $_POST['name'];
 $field_email = $_POST['email'];
 $field_message = $_POST['message'];
 
+$mail_to = 'paul.burge8@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
 
-//If all required fields are filled
-if (!empty($field_name)&&!empty($field_email)&&!empty($field_message))
-	{
-	$mail_to = 'paul.burge8@gmail.com';
-	$subject = 'Message from a site visitor: '.$field_name;
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
 
-	$body_message = 'From: '.$field_name."\n";
-	$body_message .= 'E-mail: '.$field_email."\n";
-	$body_message .= 'Message: '."\n";
-	$body_message .= $field_message;
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
 
-	$headers = 'From: '.$field_email."\r\n";
-	$headers .= 'Reply-To: '.$field_email."\r\n";
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
 
-	$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-	echo "Thank you for your message.";
-	?>
+if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert("Thanks for the message! I'll be sure to contact you as soon as I can!");
+		alert('Thank you for the message! I will get back with you as soon as possible!');
 		window.location = 'index.html';
 	</script>
 <?php
-	}
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert("The email didn't go through! Please try again.");
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
