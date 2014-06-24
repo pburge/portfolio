33ecd6384e26f246f30c365ab91e478ca43a04c7
Paul Burge <paul.burge8@gmail.com>
2014-06-23 23:56:10 -0400

    added about section and fixed contact form so it sends emails correctly

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
+// scrolls down to the projects
 $("#web").click(function() {
 	$('html,body').animate({
 	scrollTop: $("#first").offset().top},
 	'slow');
 });
 
+// scrolls down to the about section
 $("#about").click(function() {
 	$('html,body').animate({
 	scrollTop: $("#second").offset().top},
 	'slow');
 });
 
+// scrolls all the way to the bottom for the contact form
 $("#contact").click(function() {
 	$('html,body').animate({
 	scrollTop: $("#third").offset().top},
 	'slow');
 });
+
+// just sends the user back to the top
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+// fades a button in and out at above or below 1000px.
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1000) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+});
+
---
+++ php/contact.php
@@ -0,0 +1,35 @@
+<?php
+
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
+
+$mail_to = 'paul.burge8@gmail.com';
+$subject = 'Portfolio Message: '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { 
+	?>
+	<script language="javascript" type="text/javascript">
+		alert("Thanks for the message! I'll be sure to contact you as soon as I can!");
+		window.location = '../index.html';
+	</script>
+<?php
+}
+else{
+	?>
+	<script language="javascript" type="text/javascript">
+		alert("Something went wrong! Please try sending it again!");
+		window.location = '../index.html#contact';
+	</script>
+<?php
+}
+?>
