a33023601e4201193ab2377809549a37ee2586f3
Paul Burge <paul.burge8@gmail.com>
2014-09-18 05:58:27 -0400

    testing updated portfolio

--- app/app.js
+++ app/app.js
@@ -2,9 +2,9 @@ var portfolio = angular.module('pburge',['mm.foundation','ngRoute']);
 
 portfolio.config(function($routeProvider) {
 	$routeProvider.
-		when('/',{ templateUrl: 'app/templates/main.tpl' }).
+		when('/before',{ templateUrl: 'app/templates/main.tpl' }).
 		when('/gallery',{ templateUrl: 'app/templates/gallery.tpl' }).
 		when('/dev',{ templateUrl: 'app/templates/secondary.tpl' }).
-		otherwise({ redirectTo: '/' }
+		otherwise({ redirectTo: '/dev' }
 	);
 });
