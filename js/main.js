32a6800946a6d9155589d35f013bf8def708e5b3
paul burge <paul.burge8@gmail.com>
2016-07-13 15:25:00 -0400

    missed a div, broke 2 modals

--- js/main.js
+++ js/main.js
@@ -2,5 +2,32 @@
 	$('#thumbs img').click(function(){
 	    $('#largeImage').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description').html($(this).attr('alt'));
+
 	});
+	$('#thumbs2 img').click(function(){
+	    $('#largeImage2').attr('src',$(this).attr('src').replace('thumb','large'));
+	    $('#description2').html($(this).attr('alt'));
+
+	});
+	$('#thumbs3 img').click(function(){
+	    $('#largeImage3').attr('src',$(this).attr('src').replace('thumb','large'));
+	    $('#description3').html($(this).attr('alt'));
+
+	});
+	$('#thumbs4 img').click(function(){
+	    $('#largeImage4').attr('src',$(this).attr('src').replace('thumb','large'));
+	    $('#description4').html($(this).attr('alt'));
+
+	});
+	$('#thumbs5 img').click(function(){
+	    $('#largeImage5').attr('src',$(this).attr('src').replace('thumb','large'));
+	    $('#description5').html($(this).attr('alt'));
+
+	});
+	$('#thumbs6 img').click(function(){
+	    $('#largeImage6').attr('src',$(this).attr('src').replace('thumb','large'));
+	    $('#description6').html($(this).attr('alt'));
+
+	});
+
 })(jQuery);
