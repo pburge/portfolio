984a91e00597cd7c8069029240ec99cf91628410
paul burge <paul.burge8@gmail.com>
2016-07-23 13:30:09 -0400

    improxed ux on the modal, need to clean up that jquery though

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
 (function($) {
+
+	
 	$('#thumbs img').click(function(){
 	    $('#largeImage').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description').html($(this).attr('alt'));
-
+	    $('#portfolioModal1').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 	$('#thumbs2 img').click(function(){
 	    $('#largeImage2').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description2').html($(this).attr('alt'));
-
+	    $('#portfolioModal2').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 	$('#thumbs3 img').click(function(){
 	    $('#largeImage3').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description3').html($(this).attr('alt'));
-
+	    $('#portfolioModal3').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 	$('#thumbs4 img').click(function(){
 	    $('#largeImage4').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description4').html($(this).attr('alt'));
-
+	    $('#portfolioModal4').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 	$('#thumbs5 img').click(function(){
 	    $('#largeImage5').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description5').html($(this).attr('alt'));
-
+	    $('#portfolioModal5').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 	$('#thumbs6 img').click(function(){
 	    $('#largeImage6').attr('src',$(this).attr('src').replace('thumb','large'));
 	    $('#description6').html($(this).attr('alt'));
-
+	    $('#portfolioModal6').animate({
+	        scrollTop: $('html, body').offset().top
+	    }, 500);
 	});
 
 })(jQuery);
